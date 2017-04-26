60a9ffad719a30055355b836690b1eacde954a4d
Mohamed Amine MILADI <p093805@ACTIV.AVIVA.CORP>
2017-04-26 23:06:30 +0200

    oauth2 server configuration

--- auth-service/src/main/java/com/alphalog/geocoder/security/AuthApplication.java
+++ auth-service/src/main/java/com/alphalog/geocoder/security/AuthApplication.java
@@ -1,50 +1,27 @@
 package com.alphalog.geocoder.security;
 
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
 
 import com.alphalog.geocoder.security.service.security.MongoUserDetailsService;
 
 @SpringBootApplication
 @EnableResourceServer
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @ComponentScan(basePackages = "com.alphalog")
 public class AuthApplication {
@@ -54,43 +31,12 @@ public class AuthApplication {
 	}
 
 	@Configuration
-	@EnableResourceServer
-	protected static class webSecurityConfig extends ResourceServerConfigurerAdapter {
-
-		@Autowired
-		private MongoUserDetailsService userDetailsService;
-
-//		@Override
-//		protected void configure(HttpSecurity http) throws Exception {
-//			// @formatter:off
-//			http.authorizeRequests().
-//			antMatchers("/oauth/token").permitAll()
-//			.anyRequest().authenticated()
-//			.and()
-//			.csrf().disable();
-//			// @formatter:on
-//		}
-//
-//		@Override
-//		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//			auth.userDetailsService(userDetailsService);
-//					//.passwordEncoder(new BCryptPasswordEncoder());
-//		}
-//
-//		@Override
-//		@Bean
-//		public AuthenticationManager authenticationManagerBean() throws Exception {
-//			return super.authenticationManagerBean();
-//		}
-	}
-	@Configuration
 	@EnableAuthorizationServer
 	protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
 		private TokenStore tokenStore = new InMemoryTokenStore();
 
 		@Autowired
-		//@Qualifier("authenticationManagerBean")
 		private AuthenticationManager authenticationManager;
 
 		@Autowired
